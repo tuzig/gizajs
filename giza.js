093260b962b28b79382c5fdafc33c5993eb28831
benny daon <bennydaon@gmail.com>
2018-05-07 16:19:29 +0300

    pointing home to the new giza

--- giza.js
+++ giza.js
@@ -599,7 +599,7 @@ function gotoState(state, message) {
 }
 
 route('/', function() {
-    route('/%D7%92%D7%99%D7%96%D7%94%20%D7%92%D7%95%D7%9C%D7%93%D7%A4%D7%90%D7%A8%D7%91%20%D7%9C%D7%91%D7%99%D7%AA%20%D7%91%D7%A8%D7%90%D7%95');
+    route('giza');
 });
 
 route('/noya', function() {
