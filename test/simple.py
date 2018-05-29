0d13a87e361476f9e2090be3299d48088b86deb2
benny daon <bennydaon@gmail.com>
2018-05-29 23:15:18 +0300

    FAIL: test for chronus h1 visibility

--- test/simple.py
+++ test/simple.py
@@ -10,17 +10,11 @@ driver = webdriver.Firefox()
 ## my shit
 driver.get("http://127.0.0.1:8000")
 assert "גיזה" in driver.title
-try:
-        element = WebDriverWait(driver, 10).until(
-                    EC.visibility_of_element_located((By.NAME, "enter"))
-                )
-finally:
-        if not element:
-            print("'enter' elment didn't show up after 10 seconds");
-            sys.exit(1)
-elem = driver.find_element_by_name("enter")
+elem = WebDriverWait(driver, 10).until(
+           EC.visibility_of_element_located((By.NAME, "enter")))
 elem.click()
-elem = driver.find_element_by_tag_name("h1")
+elem = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.TAG_NAME, "h1")))
 assert "גיזה גולדפארב" in driver.page_source
 driver.close()
 print("PASSED")
