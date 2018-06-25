5dbacdab0992f039867d7ff20e48f8d09e6ee569
benny daon <bennydaon@gmail.com>
2018-06-25 12:08:16 +0300

    adding a threaded web server for the test

--- test/simple.py
+++ test/simple.py
@@ -1,20 +1,41 @@
 import sys
+from threading import Thread
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Firefox()
-## my shit
-driver.get("http://127.0.0.1:8000")
-assert "גיזה" in driver.title
-elem = WebDriverWait(driver, 5).until(
-           EC.visibility_of_element_located((By.NAME, "enter")))
-elem.click()
-elem = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.TAG_NAME, "h1")))
-assert "גיזה גולדפארב" in driver.page_source
-driver.close()
-print("PASSED")
+httpd = None
+
+
+def run_server(httpd):
+    sa = httpd.socket.getsockname()
+    serve_message = "Serving HTTP on {host} port {port} (http://{host}:{port}/) ..."
+    print(serve_message.format(host=sa[0], port=sa[1]))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, exiting.")
+        sys.exit(0)
+
+
+if __name__ == '__main__':
+    # server setup
+    httpd = HTTPServer(("", 8888), SimpleHTTPRequestHandler)
+    Thread(target=run_server, args=(httpd, )).start()
+
+    # the tests
+    driver = webdriver.Firefox()
+    driver.get("http://127.0.0.1:8888")
+    assert "גיזה" in driver.title
+    elem = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.NAME, "enter")))
+    elem.click()
+    elem = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.TAG_NAME, "h1")))
+    assert "גיזה גולדפארב" in driver.page_source
+    driver.close()
+    httpd.shutdown()
+    print("PASSED")
