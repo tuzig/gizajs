d24065e9e805cfd0921de2dc8f34b52a2d99b113
benny daon <bennydaon@gmail.com>
2018-07-03 18:18:21 +0300

    Fix the test server. kind of.

--- test/simple.py
+++ test/simple.py
@@ -1,4 +1,7 @@
+import os
 import sys
+from http import HTTPStatus
+import urllib
 from threading import Thread
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
@@ -10,6 +13,86 @@ from selenium.webdriver.support import expected_conditions as EC
 httpd = None
 
 PORT = 8765
+
+class WebAppRequestHandler(SimpleHTTPRequestHandler):
+    def send_head(self):
+        """Common code for GET and HEAD commands.
+
+        This sends the response code and MIME headers.
+
+        Return value is either a file object (which has to be copied
+        to the outputfile by the caller unless the command was HEAD,
+        and must be closed by the caller under all circumstances), or
+        None, in which case the caller has nothing further to do.
+
+        """
+        path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            parts = urllib.parse.urlsplit(self.path)
+            if not parts.path.endswith('/'):
+                # redirect browser - doing basically what apache does
+                self.send_response(HTTPStatus.MOVED_PERMANENTLY)
+                new_parts = (parts[0], parts[1], parts[2] + '/',
+                             parts[3], parts[4])
+                new_url = urllib.parse.urlunsplit(new_parts)
+                self.send_header("Location", new_url)
+                self.end_headers()
+                return None
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+        ctype = self.guess_type(path)
+        try:
+            f = open(path, 'rb')
+        except OSError:
+            f = open('index.html', 'rb')
+
+        try:
+            fs = os.fstat(f.fileno())
+            # Use browser cache if possible
+            if ("If-Modified-Since" in self.headers
+                    and "If-None-Match" not in self.headers):
+                # compare If-Modified-Since and time of last file modification
+                try:
+                    ims = email.utils.parsedate_to_datetime(
+                        self.headers["If-Modified-Since"])
+                except (TypeError, IndexError, OverflowError, ValueError):
+                    # ignore ill-formed values
+                    pass
+                else:
+                    if ims.tzinfo is None:
+                        # obsolete format with no timezone, cf.
+                        # https://tools.ietf.org/html/rfc7231#section-7.1.1.1
+                        ims = ims.replace(tzinfo=datetime.timezone.utc)
+                    if ims.tzinfo is datetime.timezone.utc:
+                        # compare to UTC datetime of last modification
+                        last_modif = datetime.datetime.fromtimestamp(
+                            fs.st_mtime, datetime.timezone.utc)
+                        # remove microseconds, like in If-Modified-Since
+                        last_modif = last_modif.replace(microsecond=0)
+
+                        if last_modif <= ims:
+                            self.send_response(HTTPStatus.NOT_MODIFIED)
+                            self.end_headers()
+                            f.close()
+                            return None
+
+            self.send_response(HTTPStatus.OK)
+            self.send_header("Content-type", ctype)
+            self.send_header("Content-Length", str(fs[6]))
+            self.send_header("Last-Modified",
+                self.date_time_string(fs.st_mtime))
+            self.end_headers()
+            return f
+        except:
+            f.close()
+            raise
+
 
 
 def run_server(httpd):
@@ -25,13 +108,16 @@ def run_server(httpd):
 
 if __name__ == '__main__':
     # server setup
-    httpd = HTTPServer(("", PORT), SimpleHTTPRequestHandler)
+    httpd = HTTPServer(("", PORT), WebAppRequestHandler)
     Thread(target=run_server, args=(httpd, )).start()
 
     # the tests
     driver = webdriver.Firefox()
     driver.get(f"http://127.0.0.1:{PORT}")
     assert "טוזיג" in driver.title
+    elem = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, "giza-link")))
+    elem.click()
     elem = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.NAME, "enter")))
     elem.click()
